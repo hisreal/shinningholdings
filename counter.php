cbe18613bc6ec2b4e201e5372b962ec33c72668e
hisreal <65047346+hisreal@users.noreply.github.com>
2025-11-19 17:16:16 +0100

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,46 +1,53 @@
 <!-- section content begin -->
-	<section style="margin-top:-50px;" class="py-5 in-avo-2">  
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-10 col-lg-8">
-					<h5 class="main-head"><a class="header-link" href="investments.php#overview">Explore Our Investment</a></span></h5>
-					<p style="text-align: center; font-size: 16px" class="lead text-muted">Working to deliver long-term sustainable value whilst improving lives and transforming Africa. 
-						The value of our portfolio companies as at 31 October 2025 is $1.2Million..</p>
-				</div>
-			</div>
-		</div>
-	</section>
+<section style="margin-top:-50px;" class="py-5 in-avo-2">  
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+                <h5 class="main-head">
+                    <a class="header-link" href="investments.php#overview">Explore Our Investment</a>
+                </h5>
+                <p style="text-align: center; font-size: 16px" class="lead text-muted">
+                    Working to deliver long-term sustainable value whilst improving lives and transforming Africa.
+                    The value of our portfolio companies as at 31 October 2025 is $1.2Million.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <section style="margin-top:-70px;" class="py-5 in-avo-2">  
-			<div class="container"> 
-				<div class="row justify-content-center text-center">
-					 <div class="col-md-3 col-lg-3 mb-4">
-						<h5 class="mb-0">
-							<span class="count" data-counter-end="4">4</span><span class="text-primary">+</span>
-						</h5>
-						<p class="text-muted">Sectors</p>
-					</div>
-				 <div class="col-md-3 col-lg-3 mb-4">
-						<h5 class="mb-0">
-							<span class="count" data-counter-end="1">1</span><span class="text-primary">+</span>
-						</h5>
-						<p class="text-muted">Continents</p>
-					</div>
-				 <div class="col-md-3 col-lg-3 mb-4">
-						<h5 class="mb-0">
-							<span class="count" data-counter-end="40">40</span><span class="text-primary">+</span>
-						</h5>
-						<p class="text-muted">Employess</p>
-					</div>
-				 <div class="col-md-3 col-lg-3 mb-4">
-						<h5 class="mb-0">
-							$<span class="count" data-counter-end="1">1</span>M<span class="text-primary">+</span>
-						</h5>
-						<p class="text-muted">Current Value of Portfolio Investments</p>
-					</div>
-					
-				</div>
-				
-			</div>
-		</section>
-		<!-- section content end -->
+    <div class="container"> 
+        <div class="row justify-content-center text-center">
+
+            <div class="col-md-3 col-lg-3 mb-4">
+                <h5 class="mb-0">
+                    <span class="count" data-counter-end="4">4</span><span class="text-primary">+</span>
+                </h5>
+                <p class="text-muted">Sectors</p>
+            </div>
+
+            <div class="col-md-3 col-lg-3 mb-4">
+                <h5 class="mb-0">
+                    <span class="count" data-counter-end="1">1</span><span class="text-primary">+</span>
+                </h5>
+                <p class="text-muted">Continents</p>
+            </div>
+
+            <div class="col-md-3 col-lg-3 mb-4">
+                <h5 class="mb-0">
+                    <span class="count" data-counter-end="40">40</span><span class="text-primary">+</span>
+                </h5>
+                <p class="text-muted">Employees</p>
+            </div>
+
+            <div class="col-md-3 col-lg-3 mb-4">
+                <h5 class="mb-0">
+                    $<span class="count" data-counter-end="1">1</span>M<span class="text-primary">+</span>
+                </h5>
+                <p class="text-muted">Current Value of Portfolio Investments</p>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- section content end -->
