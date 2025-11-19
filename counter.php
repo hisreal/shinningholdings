6d44e2a40973f2e51a0b8d1d3b46f3e11114d733
hisreal <65047346+hisreal@users.noreply.github.com>
2025-11-19 17:12:21 +0100

    updated

--- counter.php
+++ counter.php
@@ -3,7 +3,7 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-10 col-lg-8">
-					<h5 class="main-head">Explore Our Investment</span></h5>
+					<h5 class="main-head"><a class="header-link" href="investments.php#overview">Explore Our Investment</a></span></h5>
 					<p style="text-align: center; font-size: 16px" class="lead text-muted">Working to deliver long-term sustainable value whilst improving lives and transforming Africa. 
 						The value of our portfolio companies as at 31 October 2025 is $1.2Million..</p>
 				</div>
@@ -13,26 +13,26 @@
 
 <section style="margin-top:-70px;" class="py-5 in-avo-2">  
 			<div class="container"> 
-				<div style="text-align: center" class="row row-cols-md-3 row-cols-lg-4 text-center mb-5">
-					<div class="col">
+				<div class="row justify-content-center text-center">
+					 <div class="col-md-3 col-lg-3 mb-4">
 						<h5 class="mb-0">
 							<span class="count" data-counter-end="4">4</span><span class="text-primary">+</span>
 						</h5>
 						<p class="text-muted">Sectors</p>
 					</div>
-					<div class="col">
+				 <div class="col-md-3 col-lg-3 mb-4">
 						<h5 class="mb-0">
 							<span class="count" data-counter-end="1">1</span><span class="text-primary">+</span>
 						</h5>
 						<p class="text-muted">Continents</p>
 					</div>
-					<div class="col d-md-none d-lg-block">
+				 <div class="col-md-3 col-lg-3 mb-4">
 						<h5 class="mb-0">
 							<span class="count" data-counter-end="40">40</span><span class="text-primary">+</span>
 						</h5>
 						<p class="text-muted">Employess</p>
 					</div>
-					<div class="col">
+				 <div class="col-md-3 col-lg-3 mb-4">
 						<h5 class="mb-0">
 							$<span class="count" data-counter-end="1">1</span>M<span class="text-primary">+</span>
 						</h5>
--- index.php
+++ index.php
@@ -6,11 +6,10 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-10 col-lg-8">
-				<h5 class="main-head">About Us</h5>
+				<h5 class="main-head"><a class="header-link" href="about.php">Learn More About Us</a></h5>
 				<p style="text-align: center; font-size: 16px" class="lead text-muted">Shinning Holdings is an emerging African investment firm 
 					committed to building sustainable value across high-growth sectors. We partner with ambitious businesses and investors to unlock 
 					opportunities that shape Africa’s future.</p>
-				<center><a href="about.php" class="btn btn-primary rounded-pill">Learn More<i class="fas fa-arrow-right fa-sm ms-1"></i></a></center>
 
 			</div>
 		</div>
@@ -19,28 +18,25 @@
 
 <!-- section content end -->
 		<!-- section content begin -->
-		<section style="margin-top:-30px;" class="pt-2 pb-5 in-avo-8">
+		<section style="margin-top:-30px;" class="in-avo-8">
 			<div class="container">
 				<div class="row justify-content-center">
 					<div class="col-md-12 col-lg-10">
-					  <h5 class="main-head">What Drives Us</h5>
-						<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-2 mt-lg-5 gy-4 gy-lg-0">
-							<div class="col">
+					  <h5  class="main-head">What Drives Us</h5>
+						<div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 ">
+							<div style="padding: 5px" class="col">
 								<a  style="border: 1px solid #125a52; background-color: white"  class="card card-body text-center">
-									<i style="background-color: white" class="fas fa-seedling fa-3x text-primary"></i>
-									<h6 class="mt-n4 mb-1">Sustainable impact</h6>
+									<h6 style="font-weight: 200">Sustainable impact</h6>
 								</a>
 							</div>
-							<div class="col">
+							<div style="padding: 5px" class="col">
 								<a style="border: 1px solid #125a52" class="card card-body text-center">
-									<i class="fas fa-lightbulb fa-3x text-primary"></i>
-									<h6 class="mt-n4 mb-1">Scalable innovation</h6>
+									<h6  style="font-weight: 200">Scalable innovation</h6>
 								</a>
 							</div>
-							<div class="col">
+							<div style="padding: 5px" class="col">
 								<a style="border: 1px solid #125a52" class="card card-body text-center">
-									<i class="fas fa-handshake-angle fa-3x text-primary"></i>
-									<h6 class="mt-n4 mb-1">Trusted partnerships</h6>
+									<h6  style="font-weight: 200">Trusted partnerships</h6>
 								</a>
 							</div>
 							
@@ -51,7 +47,7 @@
 		</section>
 
  <!-- section content begin -->
-		<section style="margin-top:-50px;" class="py-5 card-style-10 in-avo-2">
+		<section style="margin-top:-10px;" class="py-5 card-style-10 in-avo-2">
 			<div class="container">
 			  <h5 class="main-head">Our Focus Area</h5>
 				<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-2 g-lg-4">
@@ -114,10 +110,11 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-md-10 col-lg-8">
-				<h5 class="main-head">FEATURED INVESTMENT : The PlugLuxx</h5>
+				<h6 class="main-head">FEATURED INVESTMENT</h6>
+					<h6 style="color:#262626; margin-top: -10px" class="main-head">The PlugLuxx</h6>
 				<p style="text-align: center; font-size: 16px" class="lead text-muted">Shinning Holdings is proud to support The PlugLuxx, a digital-first luxury marketplace focused on high-end real estate, experiential hospitality, and mobility. Through this strategic investment, 
 					we tap into Africa’s premium lifestyle economy, providing access to luxury penthouses, VIP events, supercars, and bespoke experiences. The PlugLuxx embodies our commitment to innovation, upscale real estate, and scalable digital platforms — and is a core piece of our growth story.</p>
-				<center><a href="https://theplugluxx.com/" class="btn btn-primary rounded-pill mt-2">Learn More About The PlugLuxx <i class="fas fa-arrow-right fa-sm ms-1"></i></a></center>
+				<center><a href="https://theplugluxx.com/" style="font-weight: 200" class="btn btn-primary rounded-pill">Learn More<i class="fas fa-arrow-right fa-sm ms-1"></i></a></center>
 
 			</div>
 		</div>
