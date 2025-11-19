912fe1a2b25db2a2819d98b69a35912a9456ad42
hisreal <65047346+hisreal@users.noreply.github.com>
2025-11-19 19:18:03 +0100

    Create investments copy.php

---
+++ investments copy.php
@@ -0,0 +1,148 @@
+<?php require_once("header.php"); ?>
+<?php require_once("page_loader.php"); ?>
+<?php require_once("nav-bar.php");?>
+<section class=" hero-banner">
+  <div class="container">
+    <div class="row ">
+      <div class="hero-content col-md-10 col-lg-8">
+        <h1 class="fw-bold display-4 mb-3">
+          Investment Portfolio
+        </h1>
+        
+      </div>
+    </div>
+  </div>
+</section>
+
+<section style="margin-top:-50px;" class="py-5 highlights fade-in-bottom"> 
+  <ul style="text-transform: uppercase; font-weight: 100" class="nav  justify-content-center">
+    <li style="text-transform: uppercase; font-weight: 100" class="nav-item">
+      <a class="nav-link" href="#overview">Highlights</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#power">Power</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#technology">Technology</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#energy">Energy</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#estate">Real Estate and Hospitality</a>
+    </li>
+  </ul>
+</section>
+
+
+<section id="overview" class="py-5 in-avo-2 ">  
+    <div class="container">
+        <div  class="row justify-content-center">
+            <div class="col-md-10 col-lg-8">
+                <h5 class="main-head">PORTFOLIO HIGHLIGHTS</h5>
+                <p style="text-align: center; font-size: 16px" class="lead text-muted fade-in-bottom">
+                  We invest in sectors that provide strong long-term returns and have the ability to transform Africa's economy.
+                  The value of our portfolio companies as at 31 October 2025 is $1.2Million.
+                </p>
+
+                 <h5 class="main-head">Investment Philosophy</h5>
+                <p style="text-align: center; font-size: 16px" class="lead text-muted fade-in-bottom">
+                  We operates with a clear long-term vision, investing with the future in mind rather than chasing short-term gains. Our work is driven by
+                   a strong sense of purpose, ensuring that every investment delivers both financial success and meaningful social impact. We believe deeply in
+                    the power of collaboration, co-investing, and partnering with both local and global stakeholders to build sustainable value and share long-term success.
+            </div>
+        </div>
+
+        <div id="power" class="bg-block mb-4" 
+             style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+             url('img/services/power-image.jpg'); border-radius: 15px;">
+            <div class="bg-overlay-text">
+               <h1 class="fw-bold display-4 mb-3">
+                 Power
+                </h1>
+            </div>
+        </div>
+        
+        <p style="font-size: 16px;  text-align: center" class="lead text-muted fade-in-bottom">
+         We invest in transformative power initiatives that strengthen electricity generation, 
+         modernize distribution networks, and support Africa’s shift toward cleaner, more reliable
+          energy systems. Our commitment to renewable energy, advanced grid technologies, and
+           strategic public–private collaborations ensures that communities and industries gain
+            access to stable and sustainable power.
+
+        </p>
+
+         <div id="technology" class="bg-block mb-4" 
+             style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+             url('img/services/technology-image.jpg'); border-radius: 15px;">
+            <div class="bg-overlay-text">
+               <h1 class="fw-bold display-4 mb-3">
+                 Technology
+                </h1>
+            </div>
+        </div>
+        <p style="font-size: 16px;  text-align: center" class="lead text-muted fade-in-bottom">
+       We invest in technology that drives productivity, connectivity, and economic inclusion across Africa. 
+       Our focus spans groundbreaking fintech solutions, environmentally conscious cleantech 
+       innovations, intelligent real estate technologies, and advanced digital infrastructure such
+        as AI and cloud systems. Through these investments, we empower businesses and consumers to 
+        thrive in a rapidly evolving digital landscape.
+        </p>
+
+
+
+         <div id="energy" class="bg-block mb-4" 
+             style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+             url('img/services/energy2.jpg'); border-radius: 15px;">
+            <div class="bg-overlay-text">
+               <h1 class="fw-bold display-4 mb-3">
+                 Energy
+                </h1>
+            </div>
+        </div>
+        <p style="font-size: 16px;  text-align: center" class="lead text-muted fade-in-bottom">
+        hinning Holdings is advancing Africa’s energy future through investments that enhance access,
+         efficiency, and long-term energy security. Our strategy centers on integrated solutions such
+          as gas-to-power systems, modern storage technologies, downstream infrastructure development,
+         and cleaner fuel innovations — all designed to meet the continent’s rising energy demand.
+      </p>
+           <div class="row gx-0">
+							<div style="border-top: 2px solid #125a52" class="col text-center border-bottom justify-content-md-center border-bottom-md-0 border-end-md">
+								<a href="https://theplugluxx.com/" alt="_blank"><img  class="opacity-75 my-2 my-md-1" src="img/user/plugluxx.png" alt="PlugLuxx logo" width="120" height="auto"></a>
+								<br>
+                <br>
+              </div>
+						</div>
+         <div id="estate" class="bg-block mb-4" 
+             style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), 
+             url('img/services/estate-image.jpg'); border-radius: 15px;">
+            <div class="bg-overlay-text">
+               <h1 class="fw-bold display-4 mb-3">
+                 Real Estate and Hospitality
+                </h1>
+            </div>
+        </div>
+        <p style="font-size: 16px; text-align: center" class="lead text-muted fade-in-bottom">
+        Shinning Holdings builds value in real estate and hospitality by developing modern, sustainable
+         properties while also investing in premium lifestyle experiences, travel, and luxury service 
+         platforms. From residential and commercial developments to high-end rental, concierge,
+          and experiential brands like The PlugLuxx, our combined real estate and
+         hospitality strategy enhances quality of life and elevates Africa’s premium lifestyle economy.
+        </p>
+ 
+					
+						<div class="row gx-0">
+							<div style="border-top: 2px solid #125a52" class="col text-center border-bottom justify-content-md-center border-bottom-md-0 border-end-md">
+								<a href="https://theplugluxx.com/"  alt="_blank"><img  class="opacity-75 my-2 my-md-1" src="img/user/plugluxx.png" alt="PlugLuxx logo" width="120" height="auto"></a>
+							</div>
+              
+						</div>
+			
+        <!-- Company grid goes here -->
+    </div>
+</section>
+<?php require_once("call.php"); ?>
+
+
+
+<?php require_once("footer.php"); ?>
